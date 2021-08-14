2d8ea20c21327477f99a4fac5dd1a5a7f7f7291c
cell2silicon <83265414+cell2silicon@users.noreply.github.com>
2021-08-14 17:55:16 +1000

    final update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -47,25 +47,38 @@ shapeChange.on("change", dataFilter)
 
 // Setting data filter function
 function dataFilter() {
-    // 
+    // Showing table in HTML
     tableBody.html("")
+    // Setting filter data as table data for further filtering
     filterData = tableData
+
+    // Filtering table data by date value
     date = d3.select("#datetime").property("value")
     if(date) {
     filterData = filterData.filter( r => r.datetime == date )}
+
+    // Filtering table data by city value
     city = d3.select("#city").property("value")
     if(city) {
     filterData = filterData.filter( r => r.city == city )}
+
+    // Filtering table data by state value
     state = d3.select("#state").property("value")
     if(state) {
     filterData = filterData.filter( r => r.state == state )}
+
+    // Filtering table data by country value
     country = d3.select("#country").property("value")
     if(country) {
     filterData = filterData.filter( r => r.country == country )}
+
+    // Filtering table data by shape value
     shape = d3.select("#shape").property("value")
     if(shape) {
     filterData = filterData.filter( r => r.shape == shape )}
+    
+    // Invoking function to show filtered data
     alienTable(filterData)
 }
-
+// Invoking function to show data
 alienTable(tableData);
